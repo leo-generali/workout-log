c32228a6f5cfde8c19fa56235223c87eef33722e
Leo Generali <leogen17@gmail.com>
2017-03-10 19:15:47 -0500

    Adds full date functionality

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,13 @@
 export function formatDate(date) {
 
-  const dd = date.getDate();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  const dd = date.getDay();
   const ddd = date.getDate();
   const mm = date.getMonth() + 1; 
   const yyyy = date.getFullYear();
 
-  return ddd + '/' + mm + '/' + yyyy
+  return days[dd - 1] + ', ' + months[mm] + ' ' + ddd + ' ' + yyyy; 
 
 }
